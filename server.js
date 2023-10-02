7523e5c6c332bc768b7e541233ebd29a09c08d1e
rojiloro <kelengoji79@gmail.com>
2023-10-02 23:13:18 +0700

    bismillah

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ const router = require("./src/routes/index");
 
 app.use("/api/v1/", router);
 
-app.listen(port, () => console.log(`listening on port http://localhost: ${port}`));
+app.listen(port, () => console.log(`listening on port : ${port}`));
