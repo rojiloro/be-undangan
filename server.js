86a031cbc6e89bdc376f427b9b8b34b18949228e
rojiloro <32476245+rojiloro@users.noreply.github.com>
2023-10-03 14:30:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 const req = require("express/lib/request");
-const port = process.env.DB_PORT || PORT;
+const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
