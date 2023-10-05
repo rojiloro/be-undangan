60abdc7e392c2bc14459b89f9f337ab63203115b
rojiloro <32476245+rojiloro@users.noreply.github.com>
2023-10-05 14:04:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  credential : true,
+  origin : "*",
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+}))
 
 const router = require("./src/routes/index");
 
